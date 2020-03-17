a79904704a1330b1628c07eeaeb2f7be47bf6bf5
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2020-03-17 16:24:34 +0100

    Finished week 4

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ console.log(name);
 console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = `<strong>Nazywam</strong> się ${name} i mam ${age} lat.`;
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[0]);
