ba95ef103eb12e741c597756009b8a5885d89139
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2020-04-04 21:11:10 +0200

    Changes 2

--- main.js
+++ main.js
@@ -8,10 +8,6 @@ console.log(`Nazywam się ${name} i mam ${age} lat.`);
 
 const about = document.querySelector('.about__paragraph--js');
 
-console.log(about.innerHTML);
-
-about.innerHTML = `<strong>Nazywam</strong> się ${name} i mam ${age} lat.`;
-
 const paragraphs = document.querySelectorAll('p');
 
 console.log(paragraphs);
@@ -49,7 +45,7 @@ const navigationSwitcher = document.querySelector('.navigation__switcher--js');
 
 navigationSwitcher.addEventListener('click', (e) => {
     const navigationList = document.querySelector('.navigation__list--js');
-    navigationList.classList.toggle('navigation__list--visible');    
+    navigationList.classList.toggle('navigation__list--visible');
     
     if (navigationSwitcher.innerHTML == '☰') {
         navigationSwitcher.innerHTML = '🗙';
@@ -57,3 +53,19 @@ navigationSwitcher.addEventListener('click', (e) => {
         navigationSwitcher.innerHTML = '☰';
     }        
 });
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timer').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
