d7fe1380cf462c93cd0d2f364f18da75eaa49217
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2020-05-05 19:19:38 +0200

    Changes 4

---
+++ mountains.js
@@ -0,0 +1,28 @@
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+    
+    if (navigationSwitcher.innerHTML == '☰') {
+        navigationSwitcher.innerHTML = '🗙';
+    } else {
+        navigationSwitcher.innerHTML = '☰';
+    }        
+});
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timer').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
