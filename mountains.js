37dd1bad07b5e9fe9a6f2e45874ccde9cb88147c
Tomasz Anuszczyk <tomaszanuszczyk@gmail.com>
2020-05-12 21:51:00 +0200

    Feature: add slider to rysy subpage

--- mountains.js
+++ mountains.js
@@ -26,3 +26,60 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
+
+class Slider {
+    constructor(images) {
+        this.images= images;
+        this.slide = null;
+        this.prevBtn = null;
+        this.nextBtn = null;
+        this.image = null;
+        this.currentSlide = 0;
+
+        this.UiSelectors = {
+            slide: '[data-slide]',
+            buttonPrev: '[data-button-prev]',
+            buttonNext: '[data-button-next]'
+        }
+    }
+
+    initializeSlider() {
+        this.slide = document.querySelector(this.UiSelectors.slide);
+        this.prevBtn = document.querySelector(this.UiSelectors.buttonPrev);
+        this.nextBtn = document.querySelector(this.UiSelectors.buttonNext);
+
+        this.image = document.createElement('img');
+        this.image.classList.add('slide__image');
+
+        this.setSlideAttributes(0);
+
+        this.slide.appendChild(this.image);
+
+        this.addListeners();
+    }
+
+    addListeners() {
+        this.prevBtn.addEventListener('click', () => this.changeSlide(this.currentSlide - 1));
+        this.nextBtn.addEventListener('click', () => this.changeSlide(this.currentSlide + 1));
+    }
+
+    changeSlide(index) {
+        if (index === -1) {
+            index = 2;
+        }
+
+        if (index === 3) {
+            index = 0;
+        }
+        
+
+        this.currentSlide = index;
+
+        this.setSlideAttributes(index);
+    }
+
+    setSlideAttributes(index) {
+        this.image.setAttribute('src', Array.isArray(this.images) && this.images.length && this.images[index]);
+        this.image.setAttribute('alt', `Slide ${index + 1}`);
+    }
+}
